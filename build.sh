bd97daf14b14b4b04cedcf92995b47d416a1d2db
azole <azolelai@gmail.com>
2015-07-21 14:45:05 +0800

    check phpbrew paths before running the commands

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "Shell: " $SHELL
 
 echo "Sourcing phpbrew bashrc..."
-source $HOME/.phpbrew/bashrc
+[[ -e $HOME/.phpbrew/bashrc ]] && source $HOME/.phpbrew/bashrc
 
-if [[!-e bin/phpbrew ]] ; then
+if [[ ! -e bin/phpbrew ]] ; then
     echo "Using ${PHP_VERSION}..."
     phpbrew use ${PHP_VERSION}
 fi
